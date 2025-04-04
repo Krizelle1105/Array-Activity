e09dac69850aa54bd7f1f58a9261630483fde699
Krizelle1105 <krizelleannalagadan@gmail.com>
2025-04-04 23:08:52 +0800

    Add files via upload

---
+++ Array Activity/arrayActivity.js
@@ -0,0 +1,10 @@
+let favSingers = ["Taylor Swift", "Adele", "Bruno Mars"];
+console.log("First singer:", favSingers[0]);
+
+let favNumbers = [7, 13, 21, 42];
+
+let mixedArr = ["string", ["otherarray"], 123, true];
+console.log("Item 1:", mixedArr[0]);
+console.log("Item 2:", mixedArr[1]);
+console.log("Item 3:", mixedArr[2]);
+console.log("Item 4:", mixedArr[3]);
